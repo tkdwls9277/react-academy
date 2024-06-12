45fa664f6bfff7204443784c434e3ba7c21ddf1a
Lee Sang-Jin <49237608+tkdwls9277@users.noreply.github.com>
2024-06-12 21:59:35 +0900

    height ing..

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,18 @@
-import React, { useState, useRef, useCallback, useEffect, createContext, useContext } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 import './App.css';
 import TodoBoard from "./TodoBoard";
 
-const inputText = createContext<{ testText: string, setTestResult: (newValue: string) => void }>({ testText: "", setTestResult: (newValue: string) => { } });
-
-
-
-export const useTextContext = () => {
-  const { testText } = useContext(inputText);
-
-  if (!inputText) {
-    throw new Error("provider not found" + a);
-  }
-
-  return testText
-}
-
-export const useSetTextContext = () => {
-  const { setTestResult } = useContext(inputText);
-
-  if (!inputText) {
-    throw new Error()
-  }
-
-  return setTestResult;
-}
-
-
 function App() {
   const [inputValue, setInputValue] = useState('')
-  const [todoList, setTodoList] = useState<string[]>([])
+  const [todoList, setTodoList] = useState<{ id: number, checked: boolean, content: string }[]>([])
   const inputRef = useRef<HTMLInputElement>(null)
 
   const addItem = () => {
-    setTodoList([...todoList, testText])
+    setTodoList([...todoList, { id: todoList[todoList.length].id + 1, checked: false, content: inputValue }])
+  }
+  const firstAddItems = (items: { id: number, checked: boolean; content: string }) => {
+    // setTodoList([...todoList, ...items]);
+    setTodoList([...todoList, items])
   }
   const setText = (text: string) => {
     // setInputValue(text)
@@ -41,23 +20,49 @@ function App() {
       inputRef.current.value = text;
     }
   }
-  const [testText, setTestText] = useState<string>("");
-  const text = useContext(inputText);
-  const text = useTextContext();
-  const setTestResult = useSetTextContext();
+  // useEffect(() => {
+  //   firstAddItems(testData[0])
+  // }, []) // 빈 종속성 배열 []은 효과가 한 번만 실행되도록 보장.
+  useEffect(() => {
+    if (todoList.length < testData.length) {
+      firstAddItems(testData[todoList.length]);
+    }
 
-  const setTestResult = (newValue: string) => {
-    setTestText(newValue)
-  }
+  }, [todoList])
+
 
   return (
-    <CountProvider>
-      <input value={testText} type="text" onChange={(event) => setTestResult(event.target.value)} ref={inputRef}></input>
+    <main>
+      <input value={inputValue} type="text" onChange={(event) => setInputValue(event.target.value)} ref={inputRef}></input>
       <button onClick={addItem} >add</button>
 
       <TodoBoard todoList={todoList} setText={setText}></TodoBoard>
-    </CountProvider>
+    </main>
   );
 }
 
 export default App;
+
+const testData: { id: number, checked: boolean; content: string }[] = [
+  // 개수 20개
+  { id: 1, checked: true, content: "aa" },
+  { id: 2, checked: true, content: "bb" },
+  { id: 3, checked: false, content: "cc" },
+  { id: 4, checked: true, content: "dd" },
+  { id: 5, checked: true, content: "ff" },
+  { id: 6, checked: true, content: "gg" },
+  { id: 7, checked: false, content: "hh" },
+  { id: 8, checked: false, content: "oo" },
+  { id: 9, checked: true, content: "pp" },
+  { id: 10, checked: false, content: "qq" },
+  { id: 11, checked: false, content: "rr" },
+  { id: 12, checked: false, content: "ss" },
+  { id: 13, checked: false, content: "tt" },
+  { id: 14, checked: true, content: "uu" },
+  { id: 15, checked: true, content: "vv" },
+  { id: 16, checked: true, content: "ww" },
+  { id: 17, checked: false, content: "xx" },
+  { id: 18, checked: false, content: "yy" },
+  { id: 19, checked: true, content: "zz" },
+  { id: 20, checked: true, content: "ll" },
+]
--- src/TodoBoard.tsx
+++ src/TodoBoard.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TodoItem from "./TodoItem";
 
-const TodoBoard = (props: any) => {
+const TodoBoard = (props: { setText, todoList: { id: number, checked: boolean; content: string }[] }) => {
+    const itemRef = useRef<HTMLInputElement>(null);
+    const [ddd, setDdd] = useState<{ id: number, checked: boolean, content: string, }[]>([])
+    useEffect(() => {
+        // console.log("props.todoList" + props.todoList.content)
+        // debugger;
+        if (itemRef.current) {
+            console.log("offsetHeight: " + itemRef.current.offsetHeight);
+            // setDdd([...ddd, {
+            //     id: props.todoList[props.todoList.length].id, checked: props.todoList[0].checked, content: props.todoList[0].content, paging: itemRef.current.offsetHeight / 400
+            // }])
+        }
+
+    }, [props.todoList])
+    props.todoList[0].content = "11";
+    // setDdd()
     return (
-        <div>
+        <div ref={itemRef}>
             <h1>todo list</h1>
             {props.todoList.map((item: any, index: number) => <TodoItem key={index} item={item} setText={props.setText}></TodoItem>)}
         </div>
